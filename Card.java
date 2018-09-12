3bf2cb443af461ddeed171465c02beb2ada6426b
abbyLinstone <43222224+abbyLinstone@users.noreply.github.com>
2018-09-12 18:44:02 -0400

    Update Card.java

--- Card.java
+++ Card.java
@@ -7,5 +7,24 @@ public class Card
    private enum Suit{CLUB, SPADE, DIAMOND, HEART};   
    // Face enumerated data type
    private enum Face{ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING};
+   
+   public string getSuit()
+   {
+   
+   }
+   public void setSuit()
+   {
+   
+   }
+   
+   public string getFace()
+   {
+   
+   }
+   
+   public void setFace()
+   {
+   
+   }
 
 }
