f497f6403554bf6e1c26a7aebc4e86f9dd5a4af8
abbyLinstone <43222224+abbyLinstone@users.noreply.github.com>
2018-09-24 12:58:21 -0400

    Create Hand.java

---
+++ Hand.java
@@ -0,0 +1,37 @@
+// Warm-up Project
+// CS 205
+   
+import java.util.ArrayList;
+
+public class Hand
+{
+   private ArrayList<Fish_Card> hand;
+   private Fish_Deck d = new Fish_Deck();
+   
+   public ArrayList<Fish_Card> newHand()
+   {
+      hand = new ArrayList<Fish_Card>();
+      d.shuffle();
+      d.toString();
+      for(int i = 1; i < 8; i++)
+      {
+         hand.add(d.getTopCard());
+      }
+      return hand;
+   }
+   public ArrayList<Fish_Card> addCard(Fish_Deck d)
+   {
+      hand.add(d.getTopCard());
+      return hand;
+   }
+   public String toString() 
+   {
+      String s = "";
+      
+      for(Fish_Card c: hand){
+         s += c.toString() + "  "; 
+      } 
+      return s;  
+   }
+
+} 
