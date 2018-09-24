733b19b2f397531cc4364088ca076ff1cc1881ad
abbyLinstone <43222224+abbyLinstone@users.noreply.github.com>
2018-09-24 12:54:29 -0400

    Update Card.java

--- Card.java
+++ Card.java
@@ -4,27 +4,50 @@
 public class Card
 {
    // Suit enumerated data type
-   private enum Suit{CLUB, SPADE, DIAMOND, HEART};   
+   public enum Suit{CLUB, SPADE, DIAMOND, HEART};   
    // Face enumerated data type
-   private enum Face{ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING};
+   public enum Face{ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING};
    
-   public string getSuit()
+   private  Suit suit; // The card's suit   
+   private  Face face; // The card's face value
+   
+   /**
+      The Card constructor initializes the value of the suit and face parameters.
+      @param Suit suit To hold suit value.
+      @param String face To hold face value.
+   */   
+   public Card(Suit suit, Face face)
    {
-   
+      this.suit = suit;
+      this.face = face;
    }
-   public void setSuit()
+   // Getters
+   public Suit getSuit()
    {
-   
+     return this.suit;  
+   }
+   public Face getFace()
+   {
+     return this.face;  
    }
    
-   public string getFace()
+   // Setters
+   public void setSuit(Suit suit)
    {
-   
+     this.suit = suit;  
    }
-   
-   public void setFace()
+   public void setFace(Face face)
    {
-   
+     this.face = face;
+   }
+   /**
+      The toString method converts the object values of
+      suit, and face to a text string.
+      @return String The string value of cards.
+   */  
+   public String toString() 
+   {
+      return suit + ":"  + face;    
    }
 
 }
