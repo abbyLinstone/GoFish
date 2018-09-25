95caabef437a161d729b284f1edbd54b575f8a87
abbyLinstone <43222224+abbyLinstone@users.noreply.github.com>
2018-09-25 18:48:25 -0400

    Update GoFish.java

--- GoFish.java
+++ GoFish.java
@@ -8,22 +8,44 @@ public class GoFish
    private ArrayList<Card> hand1 = new ArrayList<Card>();
    private ArrayList<Card> hand2 = new ArrayList<Card>();
    Hand hand = new Hand();
+   
    public void create2Hands()
    {
       hand1 = hand.newHand();
       hand2 = hand.newHand();
-      System.out.println(hand1.toString()); 
-      System.out.println(hand2.toString());
-
    }
    public void drawCard(ArrayList<Card> h)
    {
       hand.addCard(h);
    }
+   public void play()
+   {  
+      create2Hands();
+      int i;
+      for(i=1; i<39; i++)
+      {
+         drawCard(hand1);
+      }
+      // Boolean to create turns
+      boolean turn = true; 
+      if(turn)
+      {
+      
+         turn = false;
+      }
+      else
+      {
+      
+         turn = true;
+      }
+      System.out.println(hand1.toString()); 
+      System.out.println(hand2.toString());
+   }
    public static void main(String[] args)
    {
       GoFish game = new GoFish();
-      game.create2Hands();
+      game.play();
+      
     
    }
 
